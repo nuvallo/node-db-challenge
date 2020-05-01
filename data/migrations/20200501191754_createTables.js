39d7fb8f1df58f9a7e8eff077ed5391ca7cd9cf3
nuvallo <nuvallo.inc@gmail.com>
2020-05-01 19:27:54 -0400

    resources table created

--- data/migrations/20200501191754_createTables.js
+++ data/migrations/20200501191754_createTables.js
@@ -5,6 +5,11 @@ exports.up = async function (knex) {
     table.string("description");
     table.boolean("completed").notNullable().defaultTo(false);
   });
+  await knex.schema.createTable("resources", (table) => {
+    table.increments();
+    table.string("name").notNullable().unique();
+    table.string("description");
+  });
 };
 
 exports.down = async function (knex) {
